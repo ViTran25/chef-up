2a2b62ab7d0c77e2061af66c0f72ff60f52d883c
vi-tran25 <dragond2002@gmail.com>
2024-02-03 12:10:21 -0500

    Delete incrementCounter() method

--- test1/lib/main.dart
+++ test1/lib/main.dart
@@ -67,19 +67,7 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   final List<Marker> markers = [];
   List<LatLng> routeCoordinates = [];
-  int _counter = 0;
-
-  
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+ 
   // This is the method for getting a route
   Future<void> fetchRoute() async {
     // Start point and end point

4e645315e4a2020156a20dd88206d28f5483965b
XianyuZhao04 <sammyzhao13@gmail.com>
2024-02-03 12:22:14 -0500

    geolocator

--- test1/lib/main.dart
+++ test1/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:latlong2/latlong.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:geolocator/geolocator.dart';
-import 'package:latlong2/latlong.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 void main() {
@@ -11,46 +11,21 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'ChefUp',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works forge code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'ChefUp'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -60,56 +35,197 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
-@override
-Widget build(BuildContext context) {
+  @override
+  Widget build(BuildContext context) {
 
-  return Scaffold(
+    return Scaffold(
+      appBar: AppBar(
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          //
+          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+          // action in the IDE, or press "p" in the console), to see the
+          // wireframe for each widget.
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              child: const Text('Trucker!'),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const Truckview(title: "truck")),
+                );
+              }
+            ),
+
+            Text("    "),
+
+            ElevatedButton(
+              child: const Text('User!'),
+              onPressed: () {
+                // Navigate to second route when tapped.
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const Mapview(title: "map")),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Truckview extends StatefulWidget {
+  const Truckview({super.key, required this.title});
+  final String title;
+
+  @override
+  State<Truckview> createState() => _Truckview();
+}
+
+class _Truckview extends State<Truckview> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+    );
+  }
+}
+
+class Mapview extends StatefulWidget {
+  const Mapview({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<Mapview> createState() => _Mapview();
+}
+
+class _Mapview extends State<Mapview> {
+    late Position _currentLocation;
+    late bool servicePermission = false;
+    late LocationPermission permission;
+
+    void initState() {
+    super.initState();
+    _getCurrentLocation().then((position) {
+      setState(() {
+        _currentLocation = position;
+      });
+    });
+  }
+
+
+    Future<Position> _getCurrentLocation() async {
+      servicePermission = await Geolocator.isLocationServiceEnabled();
+
+      if(servicePermission == false)
+      {
+        print("Service Disabled");
+      }
+
+      permission = await Geolocator.checkPermission();
+
+      if(permission == LocationPermission.denied)
+      {
+        permission = await Geolocator.requestPermission();
+      }
+
+      return await Geolocator.getCurrentPosition();
+    }
+
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return Scaffold(
     //Add Header if needed
   
+      //Main Body for the Web, note that it is written as a column
+      body: Column(
+        //Direction of the column orientation
+        verticalDirection: VerticalDirection.down,
 
-    //Main Body for the Web, note that it is written as a column
-    body: Column(
-      //Direction of the column orientation
-      verticalDirection: VerticalDirection.down,
-
-        //Items in the column
+          //Items in the column
         children: [
           //First item in the column, the flutter map
           Container(
             width:5000,
-            height: 500,
+            height: 496,
             child: FlutterMap(
-                   options: MapOptions(
-                   initialCenter: LatLng(40.443490, -79.941640),
-                   initialZoom: 17.2,
-    ),
-    children: [
-      TileLayer(
-        urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-        userAgentPackageName: 'com.example.app',
-      ),
-      RichAttributionWidget(
-        attributions: [
-          TextSourceAttribution(
-            'OpenStreetMap contributors',
-            onTap: () => launchUrl(Uri.parse('https://openstreetmap.org/copyright')),
+              
+              options: MapOptions(
+                initialCenter: LatLng(_currentLocation!.latitude, _currentLocation!.longitude),
+                initialZoom: 17.2,
+              ),
+              children: [
+                TileLayer(
+                  urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                  userAgentPackageName: 'com.example.app',
+                ),
+                RichAttributionWidget(
+                  attributions: [
+                    TextSourceAttribution(
+                      'OpenStreetMap contributors',
+                      onTap: () => launchUrl(Uri.parse('https://openstreetmap.org/copyright')),
+                    ),
+                  ],
+                ),
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: ElevatedButton(
+                    child: const Text('Home'),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    }
+                  ),
+                ),
+                Align(alignment: Alignment.topRight,
+                  child: ElevatedButton(
+                    child: Text('getPos'),
+                    onPressed: () async {
+                      _currentLocation = await _getCurrentLocation();
+                    },))
+              ],
+            )
           ),
-        ],
-      ),],
-      )
-  ),
 
 
-//The "Menu" for all the food trucks with their pictures and title and descriptions.
-//Edit the containers to will
-      Container(
-        //Dimensions for the ListView
-        width: double.infinity,
-        height: 300,
+      //The "Menu" for all the food trucks with their pictures and title and descriptions.
+      //Edit the containers to will
+          Container(
+            //Dimensions for the ListView
+            width: double.infinity,
+            height: 300,
 
-        child: ListView(
-          //Items in the ListView
-          children: [
+            child: ListView(
+              //Items in the ListView
+              children: [
+
             ElevatedButton(
               onPressed: (){},
               style: ButtonStyle(
@@ -144,7 +260,7 @@ Widget build(BuildContext context) {
             ElevatedButton(
               onPressed: (){},
               style: ButtonStyle(
-                  backgroundColor: MaterialStatePropertyAll<Color>(Colors.red),
+                  backgroundColor: MaterialStatePropertyAll<Color>(Colors.white),
                   minimumSize: MaterialStateProperty.all(Size(130, 40)),
                   elevation: MaterialStateProperty.all(0),
                   shape: MaterialStateProperty.all<RoundedRectangleBorder>(
@@ -165,87 +281,81 @@ Widget build(BuildContext context) {
                             Text("Miwa Kasumi", style: const TextStyle(fontWeight: FontWeight.bold)), //Name of Food truck
                             Text("      "),
                             Text("The greatest Jujutsu Sorcerer of all time.") //Description of food truck
-])
-]
-          )
-        )
-      ),
-          
-            ElevatedButton(
-              onPressed: (){},
-              style: ButtonStyle(
-                  backgroundColor: MaterialStatePropertyAll<Color>(Colors.red),
-                  minimumSize: MaterialStateProperty.all(Size(130, 40)),
-                  elevation: MaterialStateProperty.all(0),
-                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                  RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20.0),)),
-                  ),
-              child: 
-                Container(
-                  width: 5000,
-                  height: 100,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      Image(image: AssetImage('images/MM.jpg')),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                            Text("Miwa Kasumi", style: const TextStyle(fontWeight: FontWeight.bold)), //Name of Food truck
-                            Text("      "),
-                            Text("The greatest Jujutsu Sorcerer of all time.") //Description of food truck
-])
-]
-          )
-        )
-      ),
-
-
-            ElevatedButton(
-              onPressed: (){},
-              style: ButtonStyle(
-                  backgroundColor: MaterialStatePropertyAll<Color>(Colors.red),
-                  minimumSize: MaterialStateProperty.all(Size(130, 40)),
-                  elevation: MaterialStateProperty.all(0),
-                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                  RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20.0),)),
-                  ),
-              child: 
-                Container(
-                  width: 5000,
-                  height: 100,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      Image(image: AssetImage('images/MM.jpg')),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                            Text("Miwa Kasumi", style: const TextStyle(fontWeight: FontWeight.bold)), //Name of Food truck
-                            Text("      "),
-                            Text("The greatest Jujutsu Sorcerer of all time.") //Description of food truck
-])
-]
-          )
-        )
-      ),
-
-
-
-
-
-
-
-
-          ],
-        ),
-      )
-
-      ]
+        ]
     )
-  );
-}
+]
+          )
+        )
+      ),
 
+            ElevatedButton(
+              onPressed: (){},
+              style: ButtonStyle(
+                  backgroundColor: MaterialStatePropertyAll<Color>(Colors.green),
+                  minimumSize: MaterialStateProperty.all(Size(130, 40)),
+                  elevation: MaterialStateProperty.all(0),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20.0),)),
+                  ),
+              child: 
+                Container(
+                  width: 5000,
+                  height: 100,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Image(image: AssetImage('images/MM.jpg')),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                            Text("Miwa Kasumi", style: const TextStyle(fontWeight: FontWeight.bold)), //Name of Food truck
+                            Text("      "),
+                            Text("The greatest Jujutsu Sorcerer of all time.") //Description of food truck
+        ]
+    )
+]
+          )
+        )
+      ),
+
+            ElevatedButton(
+              onPressed: (){},
+              style: ButtonStyle(
+                  backgroundColor: MaterialStatePropertyAll<Color>(Colors.grey),
+                  minimumSize: MaterialStateProperty.all(Size(130, 40)),
+                  elevation: MaterialStateProperty.all(0),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20.0),)),
+                  ),
+              child: 
+                Container(
+                  width: 5000,
+                  height: 100,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Image(image: AssetImage('images/MM.jpg')),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                            Text("Miwa Kasumi", style: const TextStyle(fontWeight: FontWeight.bold)), //Name of Food truck
+                            Text("      "),
+                            Text("The greatest Jujutsu Sorcerer of all time.") //Description of food truck
+        ]
+    )
+]
+          )
+        )
+      ),
+
+
+              ],
+            ),
+          )
+        ]
+      )
+    );
+  }
 }
